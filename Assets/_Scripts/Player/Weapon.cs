69d0649c0ad37167b057c13c881d76d61cd51d4f
hoangtongvu <hoangtongvu@gmail.com>
2024-01-23 15:42:24 +0700

    Add Sword Trail PS.

--- Assets/_Scripts/Player/Weapon.cs
+++ Assets/_Scripts/Player/Weapon.cs
@@ -6,22 +6,26 @@ using UnityEngine;
 public class Weapon : SaiMonoBehaviour
 {
     [SerializeField] private Transform hitbox;
+    [SerializeField] private ParticleSystem onAttackPS;
 
     protected override void LoadComponents()
     {
         base.LoadComponents();
         this.LoadTransformInChildrenByName(out this.hitbox, "Hitbox");
+        this.LoadComponentInChildren(ref this.onAttackPS);
     }
 
 
     public void DisableWeaponCollider()
     {
         this.hitbox.gameObject.SetActive(false);
+        this.onAttackPS.Stop();
     }
 
     public void EnableWeaponCollider()
     {
         this.hitbox.gameObject.SetActive(true);
+        this.onAttackPS.Play();
     }
 
 }

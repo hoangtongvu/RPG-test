19901b17e3eae085258c100e66c5a19037b438a5
hoangtongvu <hoangtongvu@gmail.com>
2024-01-26 15:02:44 +0700

    Add Abstract class drawer in inspector.
    - Add Type Extension methods.
    - Add Object Extension methods.

---
+++ Assets/_Scripts/Core/Extensions/ObjectExtention.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Core.Extensions
+{
+    public static class ObjectExtention
+    {
+        public static bool IsGenericList(this object o)//tag NOT USING.
+        {
+            var oType = o.GetType();
+            return oType.IsGenericType && (oType.GetGenericTypeDefinition() == typeof(List<>));
+        }
+    }
+}
---
+++ Assets/_Scripts/Core/Extensions/TypeExtentions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public static class TypeExtentions
+{
+    public static bool IsList(this Type t) => t.IsGenericType && t.GetGenericTypeDefinition() == typeof(List<>);
+}
+
